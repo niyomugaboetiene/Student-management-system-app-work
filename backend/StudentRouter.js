fa499ad684f2fb27c8390634a3b65e2a6c2612fd
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-02-26 15:47:20 +0200

    first commit of Student management system

--- backend/StudentRouter.js
+++ backend/StudentRouter.js
@@ -3,3 +3,11 @@ import connection from "./connection.js"
 
 const router = express.Router()
 
+router.post('/addStudent', async(req, res) => {
+    try {
+       const { student_name, student_gender, student_age } = req.body;
+
+       if (!student_age || !student_age || !student_gender || !student_name)
+
+    }
+})
