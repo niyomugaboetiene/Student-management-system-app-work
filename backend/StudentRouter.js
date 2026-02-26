75dfa991770427f9ac11caec8d621731407c01ad
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-02-26 15:54:14 +0200

    first commit of Student management system

--- backend/StudentRouter.js
+++ backend/StudentRouter.js
@@ -7,7 +7,23 @@ router.post('/addStudent', async(req, res) => {
     try {
        const { student_name, student_gender, student_age } = req.body;
 
-       if (!student_age || !student_age || !student_gender || !student_name)
+       if (!student_age || !student_gender || !student_name) {
+         res.status(403).json({ message: 'Some fields are missing' });
+       }
 
+      connection.beginTransaction()
+
+      const [result] =  await connection.query(
+        'INSERT INTO student(student_name, student_gender, student_age) VALUES(?, ?, ?)',
+        [student_name, student_gender, student_age]
+       );
+
+      if (result.ok) {
+        return res.status(200).json({ message: 'User inserted successfully', user: result.insertId})
+      }
+    } catch (err) {
+        return res.status(201).json({ error: err.message });
     }
 })
+
+export default router
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 import express from "express"
 import  cors from "cors"
+import StudentRouter from "./StudentRouter.js";
 
 const app = express()
 app.use(express.json())
 app.use(cors())
+
+// handling middleware
+app.use('/student', StudentRouter)
 
 const PORT = 5000
 
