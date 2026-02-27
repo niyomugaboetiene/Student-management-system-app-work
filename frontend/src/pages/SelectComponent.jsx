4d68de98d8e5a14282f92d2da48de991112236d4
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-02-27 11:13:51 +0200

    Select component

--- frontend/src/pages/SelectComponent.jsx
+++ frontend/src/pages/SelectComponent.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Router, Link } from "react-router-dom";
 
 const SelectComponent = () => {
     const [studentInfo, setStudentInfo] = useState([]);
@@ -8,8 +9,10 @@ const SelectComponent = () => {
 
 
     useEffect(() => {
+
+      const fetchStudentInfo = async() => {
         try {
-            const res = axios.get("http://localhost:5000/student/studentList", {withCredentials: true}); 
+            const res = await axios.get("http://localhost:5000/student/studentList", {withCredentials: true}); 
 
             if (res.data.user.length === 0) {
                 setMessage("No student in system");
@@ -20,26 +23,33 @@ const SelectComponent = () => {
         } catch (err) {
             setError(err.message);
         }
+     }
+
+     fetchStudentInfo()
     }, []);
 
     return (
         <div>
             <table border={2}>
+                <thead>
                 <tr>
                     <th>Student id</th>
                     <th>Student gender</th>
                     <th>Student age</th>
                 </tr>
+                </thead>
 
-         <tr>
-             {studentInfo.map((user, id) => {
-                <>
+              <tbody>
+             {studentInfo.map((user, id) => (
+               <tr key={id}>
                   <td>{user.student_id}</td>
                   <td>{user.student_gender}</td>
                  <td>{user.student_age}</td>
-                </>
-             })}
-          </tr>
+                 <Link to={`/update/${user.student_id}`}>Update</Link>
+                 <Link to={`/delete/${user.student_id}`}>Delete</Link>
+               </tr>
+             ))}
+              </tbody>
             </table>
         </div>
     )
