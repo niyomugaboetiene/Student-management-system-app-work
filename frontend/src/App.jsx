bc9633812f4b346103a2cc068a5d51a44793841b
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-02-27 11:20:18 +0200

    Select component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import InsertComponent from './pages/InsertComponent'
 import SelectComponent from './pages/SelectComponent'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      {/* <InsertComponent /> */}
-      <SelectComponent />
-    </div>
+      <BrowserRouter>
+         <Routes>
+          <Route path='/' element={<InsertComponent />}/>
+          <Route path='/studentList' element={<SelectComponent />}/>
+         </Routes>
+      </BrowserRouter>
   )
 }
 
