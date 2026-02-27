20b3f1be82d49727574240517d321259f50ec0ba
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-02-27 11:48:31 +0200

    Select component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import InsertComponent from './pages/InsertComponent'
 import SelectComponent from './pages/SelectComponent'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import UpdateComponent from './pages/UpdateComponent'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
          <Routes>
           <Route path='/' element={<InsertComponent />}/>
           <Route path='/studentList' element={<SelectComponent />}/>
+          <Route path='/update/:student_id' element={<UpdateComponent />}/>
          </Routes>
       </BrowserRouter>
   )
--- frontend/src/pages/UpdateComponent.jsx
+++ frontend/src/pages/UpdateComponent.jsx
@@ -6,19 +6,60 @@ const UpdateComponent = () => {
    const [student_name, setStudent_name] = useState("");  
    const [student_gender, setStudent_gender] = useState("");  
    const [student_age, setStudent_age] = useState("");  
+   const [student_ids, setStudent_id] = useState("");  
    const [message, setMessage] = useState("");
    const [error, setError] = useState("");
-   const student_id = useParams();
+   const { student_id } = useParams();
 
     useEffect(() => {
-        const handleSelectUser = async() => {
+        const HandleSelectUser = async() => {
             try {
                const res = await axios.get(`http://localhost:5000/student/studentList/${student_id}`);
-               if (res.data.user.length == 0) {
-                setMessage("No one here")
+               if (res.data.user.length === 0) {
+                setMessage("No one here");
                }
-            }
 
+               setStudent_name(res.data.user.student_name)
+               setStudent_age(res.data.user.student_age)
+               setStudent_gender(res.data.user.student_gender)
+               setStudent_id(res.data.user.student_id)
+            } catch (err) {
+            setError(err.message);   
         }
-    })
+        } 
+
+        HandleSelectUser();
+    }, []);
+
+    return (
+     <div>
+        <div>
+            <label htmlFor="">Student Id</label>
+            <input type="text" value={student_ids} onChange={(e) => setStudent_id(e.target.value)} readOnly/>
+        </div>
+        <div>
+            <label htmlFor="">Student name</label>
+            <input type="text" value={student_name} onChange={(e) => setStudent_name(e.target.value)}/>
+        </div>
+        <div>
+            <label htmlFor="">Student gender</label>
+            <input type="text" value={student_gender} onChange={(e) => setStudent_gender(e.target.value)}/>
+        </div>
+        <div>
+            <label htmlFor="">Student age</label>
+            <input type="text" value={student_age} onChange={(e) => setStudent_age(e.target.value)}/>
+        </div>
+
+        <button>Update</button>
+
+        {message && (
+            <p>{message}</p>
+        )}
+        {error && (
+            <p>{error}</p>
+        )}
+    </div>
+    )
 }
+
+export default UpdateComponent
